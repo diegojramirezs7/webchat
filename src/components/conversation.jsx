0eed5b00ddd19bc00b90b3d8a023fa5978114958
Diego Ramirez <diegojramirezs7@gmail.com>
2022-02-08 22:22:39 -0800

    Update conversation.jsx

--- src/components/conversation.jsx
+++ src/components/conversation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Typography } from "@mui/material";
+import { authContext } from "../services/auth";
 
 export default function Conversation(props) {
     return (<div> </div>);
